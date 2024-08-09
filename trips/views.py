763263b582c8f90d0899905c402bb91b91b2de18
Sanchi Bansal <sanchibansal340@gmail.com>
2024-08-10 00:49:45 +0530

    update. queryset to filter only trips which have start date > creent date

--- trips/views.py
+++ trips/views.py
@@ -1,6 +1,7 @@
 from .models import Trip
 from .serializers import TripSerializer
 from rest_framework import generics, filters
+from django.utils import timezone
 
 # Create your views here.
 class TripDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -8,7 +9,11 @@ class TripDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TripSerializer
 
 class TripListAPIView(generics.ListCreateAPIView):
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['start_date']
+
+    # return only the trips with start start date > today's date
+    def get_queryset(self):
+        today = timezone.now().date()  # Get today's date without time
+        return Trip.objects.filter(start_date__gt=today)

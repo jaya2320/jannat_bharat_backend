e55f3d5128fc715b8f403b52173207cfaec491e7
Jaya Gupta <jayagupta752@gmail.com>
2024-08-26 20:27:54 +0530

    Jaya/Sanchi | Update settings.py

--- jannat_bharat_backend/settings.py
+++ jannat_bharat_backend/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangostatic')
+STATIC_URL = '/djangostatic/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL = '/mediafiles/'
 
 
 # Quick-start development settings - unsuitable for production

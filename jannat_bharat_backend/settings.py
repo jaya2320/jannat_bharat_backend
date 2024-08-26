846528eb973a85ea1e3f4dc7577a3aa7f16ad3cd
Jaya Gupta <jayagupta752@gmail.com>
2024-08-26 20:42:57 +0530

    Jaya/Sanchi | Update settings.py

--- jannat_bharat_backend/settings.py
+++ jannat_bharat_backend/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'djangostatic')
 STATIC_URL = '/djangostatic/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'djangomedia')
+MEDIA_URL = '/djangomedia/'
 
 
 # Quick-start development settings - unsuitable for production
